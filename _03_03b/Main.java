ba60221f51cb661b4dcf51d6dd9e93e0606928ab
Reed Atwood <82432559+atwoodreed1Git@users.noreply.github.com>
2025-10-16 03:13:03 +0000

    Add second attempt

--- _03_03b/Main.java
+++ _03_03b/Main.java
@@ -9,6 +9,16 @@ public class Main {
     System.out.println("Pass: " + gradingSystem.isAPass(percentage));
     System.out.println("Grade: " + gradingSystem.getGrade(percentage));
     System.out.println(gradingSystem.retakeMessage(percentage, true));
+
+    int[] percentages = { 99, 90, 89, 80, 79, 70, 69, 60, 59 };
+    String[] grades = { "A", "A", "B", "B", "C", "C", "D", "D", "F" };
+
+    for (int i = 0; i < grades.length; i++) {
+      System.out.println(
+          "Percent: " + percentages[i] +
+              " - Expect: " + grades[i] +
+              " - Acutal: " + gradingSystem.getGrade(percentages[i]));
+    }
   }
 
 }
